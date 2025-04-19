d01d6a7fe20ddc1f5c1495b69f447cfd206a999a
KneesDev <kneesdev@naver.com>
2025-04-19 20:36:29 +0300

    fix: declare global (no-var)

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-const prismaClientSingleton = () => {
-    return new PrismaClient();
-};
-
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-export default prisma;
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma
+}
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+export default prisma
